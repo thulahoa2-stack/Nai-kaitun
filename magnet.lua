9e09114e60056fc46d63ee6588eb2a718a28d076
thulahoa2-stack <thulahoa2@gmail.com>
2026-07-18 19:36:08 +0700

    Create magnet.lua

---
+++ magnet.lua
@@ -0,0 +1,17 @@
+getgenv().SettingFarm = {
+    ["Farm"] = {
+        ["Team"] = "Pirates", 
+        ["FPS"] = false, 
+        ["White Screen"] = false, 
+        ["Bring Mob"] = true,
+        ["Bring Mob Distance"] = 350,
+        ["Magnet Mob"] = true,
+        ["Fast Attack"] = true,
+        ["No Clip"] = true,
+    },
+    ["Race"] = { ["RGB Haki"] = true },
+    ["Sword"] = { ["Cursed Dual Katana"] = true }, 
+    ["Gun"] = { ["Soul Guitar"] = true }
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RealHaRu/HaruHub/main/HaruHubBf.lua"))()
